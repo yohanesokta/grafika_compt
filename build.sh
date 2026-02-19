ccb67ff53f78f192abb61e5d5b8de10b2763305d
yohanesokta <yohanesoktayt72@gmail.com>
2026-02-19 11:13:35 +0700

    chore: enhance build script documentation with detailed usage instructions

--- build.sh
+++ build.sh
@@ -1,4 +1,26 @@
 #!/usr/bin/env bash
+
+# ====================================================
+#  Project : GLUTApp
+#  Author  : Yohanes Oktanio
+#  NIM     : 24-095
+#  Website : yohanes.oktanio.dev
+#
+#  Deskripsi :
+#  Script ini nge‑setup & nge‑build pake CMake
+#  Kalo udah, kamu bisa langsung jalanin app‑nya
+#
+#  Usage:
+#    ./build.sh setup          # init project (doang!)
+#    ./build.sh                # build all
+#    ./build.sh main           # build + run 'main' (main nya sesuaikan ya nama executablenya!)
+#
+#  Message :
+#  Mesakno developere Mas/Mbak!
+#  Jajal, tukokno, terus oleh maem
+#  Kudu lancar, gak boleh error!
+# ====================================================
+
 set -e 
 
 if [ "$1" = "setup" ]; then
